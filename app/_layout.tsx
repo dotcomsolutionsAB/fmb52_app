0f8ef0f5199d583bc78c78e53484a92f929de6ac
Huzefa Chaiwala <hchai52@gmail.com>
2024-12-06 22:25:37 +0530

    added icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout() {
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hideAsync();
-    }, 10000);
+    }, 2000);
   }, []);
   return (
     <Stack>
