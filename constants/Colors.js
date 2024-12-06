33092a460f92a36b11906d0a38797ffd28534452
Huzefa Chaiwala <hchai52@gmail.com>
2024-12-06 22:18:29 +0530

    added sp;ash

---
+++ constants/Colors.js
@@ -0,0 +1,11 @@
+export const Color = {
+  light: {
+    primary: "#efe4ca", //light:active
+    accent: "#cca753", //normal brown
+    hover: "#f7f2e5", //light:hover
+    background: "#faf6ee", //light
+    alert: "#FD5F5F", //alert message
+    success: "#27B124", //success message
+    grey: "#807F7C", //grey
+  },
+};
