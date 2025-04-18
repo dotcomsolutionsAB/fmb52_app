d3115dc1b682255d134d67014afd34d5e99fc5bf
arwachaiwala@gmail.com <mustafachaiwala2003@gmail.com>
2025-04-18 12:34:25 +0530

    dashboard screen initiaited

--- components/cards/UserDashboardCard.tsx
+++ components/cards/UserDashboardCard.tsx
@@ -15,16 +15,19 @@ export default function () {
     <View style={styles.container}>
       <View style={{ gap: 10 }}>
         <Image source={require("@/assets/images/profile.png")} />
-        <InlineButton title="Edit" inverted={true} />
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'#1976D2',fontSize:16,fontWeight:'600'}}>HOF</Text>
+        </View>
       </View>
       <View style={{ flex: 1 }}>
-        <Text style={styles.title}>Huzefa Ahmed bhai Chaiwala</Text>
+        <Text style={[styles.title,{color:'#808080',fontSize:16}]}>Huzefa Ahmed bhai Chaiwala</Text>
         <View style={{ height: 5 }} />
-        <Text style={styles.subtitle}>ITS : {"4045XXXX"}</Text>
-        <Text style={styles.subtitle}>Mobile : {"+917003XXXXXX"}</Text>
-        <Text style={styles.subtitle}>Sector : {"Burhani"}</Text>
-        <Text style={styles.subtitle}>Sub Sector : {"B"}</Text>
-        <Text style={styles.subtitle}>Family Members : {"7"}</Text>
+        <Text style={[styles.subtitle,{color:'#CBA652',fontSize:16,fontWeight:'bold'}]}>Sector:  {"Burhani"}</Text>
+        <Text style={[styles.subtitle,{color:'#CBA652',fontSize:16,fontWeight:'bold'}]}>Sub Sector : {"H07"}</Text>
+        <View style={{flex:1,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+        <Text style={[styles.subtitle,{color:'#808080',fontSize:16,fontWeight:'bold'}]}>Hub:  {"10,000"}</Text>
+        <Text style={[styles.subtitle,{color:'#27B124',fontSize:16,fontWeight:'bold'}]}>Paid : {"5,000"}</Text>
+        </View>
       </View>
     </View>
   );
@@ -35,17 +38,19 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     borderRadius: 10,
-    backgroundColor: Colors.light.accent,
+    backgroundColor: "white",
     padding: 10,
     gap: 10,
+    borderWidth:1,
+    borderColor:'grey'
   },
   title: {
     fontSize: 18,
     fontWeight: "bold",
-    color: Colors.light.white,
+    color: "black",
   },
   subtitle: {
     fontSize: 15,
-    color: Colors.light.white,
+    color: "black",
   },
 });
