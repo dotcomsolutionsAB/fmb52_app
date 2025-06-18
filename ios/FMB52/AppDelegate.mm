3a4eb25396f52e8ce47eb63c43ead0fe25474c13
Huzefa Chaiwala <hchai52@gmail.com>
2025-06-18 19:00:01 +0530

    reomved firebase from ios

--- ios/FMB52/AppDelegate.mm
+++ ios/FMB52/AppDelegate.mm
@@ -2,7 +2,6 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
-#import <FirebaseCore/FirebaseCore.h>
 
 @implementation AppDelegate
 
@@ -13,7 +12,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  [FIRApp configure];
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
